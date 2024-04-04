e782d752306a487d0654002aad8700168acea368
vboxuser <vboxuser@linux1.myguest.virtualbox.org>
2024-04-04 11:34:06 +0300

    Added comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,9 @@
 
 int main()
 {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name; // write your name
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
